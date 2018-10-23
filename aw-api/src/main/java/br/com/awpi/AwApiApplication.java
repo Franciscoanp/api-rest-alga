77743de5b956287cbf4a7312ce6feaf616f36dd2
Francisco Pereira <francisco.anpereira@gmail.com>
2018-10-23 20:33:42 -0300

    projeto criado

---
+++ aw-api/src/main/java/br/com/awpi/AwApiApplication.java
@@ -0,0 +1,12 @@
+package br.com.awpi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwApiApplication.class, args);
+	}
+}
